8dbe8d2b92db960569a4a4893b09ecf17961f8c4
Luuk-Gunnewijk <115221003+Luuk-Gunnewijk@users.noreply.github.com>
2026-06-08 17:00:46 +0200

    AI get in correct POS

--- Dead-Mans-Trade/Assets/Scripts/Ai/Enemies/AIMovementNB.cs
+++ Dead-Mans-Trade/Assets/Scripts/Ai/Enemies/AIMovementNB.cs
@@ -6,9 +6,8 @@ public class AIMovementNB : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
 
-    [Header("ShootPOS")]
+    [Header("ShootPOS && Player")]
     [SerializeField] private Transform[] inShootPOS;
-
     [SerializeField] private Transform player;
 
     private Vector2 _inSightPOS;
@@ -32,13 +31,21 @@ public class AIMovementNB : MonoBehaviour
 
     private void Movement()
     {
+        Transform closestShootPos = null;
+        float closestDistance = Mathf.Infinity;
+
         for (int i = 0; i < inShootPOS.Length; i++)
         {
-            
+            float dist = Vector2.Distance(inShootPOS[i].position, transform.position);
+            if (dist < closestDistance)
+            {
+                closestDistance = dist;
+                closestShootPos = inShootPOS[i];
+            }
         }
-        /*Vector2 currentPlayerPOS = player.position;
-        _inSightPOS = new Vector2(player.position.x + 4, player.position.y);
-        transform.position = Vector2.MoveTowards(transform.position, _inSightPOS, moveSpeed * Time.deltaTime);*/
+        if (closestShootPos == null) return;
+        _inSightPOS = closestShootPos.position;
+        transform.position = Vector2.MoveTowards( transform.position, _inSightPOS, moveSpeed * Time.deltaTime);
     }
 
     private void CheckPlayerRotation()
